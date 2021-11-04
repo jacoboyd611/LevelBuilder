8b0e0c542629a99be681773c5f8016cace3facef
Jacob Boyd <boydj99@live.com>
2021-11-03 21:10:57 -0500

    Add project files.

---
+++ LevelBuilder/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿
+namespace LevelBuilder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.GreenYellow;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.DarkRed;
+            this.label2.Location = new System.Drawing.Point(606, 144);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 86);
+            this.label2.TabIndex = 109;
+            this.label2.Text = "1";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ LevelBuilder/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace LevelBuilder
+{
+    public partial class Form1 : Form
+    {   
+        //this will rename the file according to the number put in
+        int lvlNum = 1;
+
+        #region for testing 
+        Brush red = new SolidBrush(Color.Red);
+        List<int> brickX = new List<int>();
+        List<int> brickY = new List<int>();
+        List<int> brickHeight = new List<int>();
+        List<int> brickWidth = new List<int>();
+        #endregion
+
+        public Form1()
+        {
+            InitializeComponent();
+            CreateXml();
+
+            //testing purposes
+            ReadXml();
+            Refresh();
+        }
+
+        private void CreateXml()
+        {
+            //random setup shit that works?
+            XmlDocument doc = new XmlDocument();
+            XmlNode docNode = doc.CreateXmlDeclaration("1.0", "UTF-8", null);
+            doc.AppendChild(docNode);
+
+            //root node
+            XmlNode bricksNode = doc.CreateElement("bricks");
+            doc.AppendChild(bricksNode);
+
+            foreach (Label label in Controls.OfType<Label>())
+            {
+                label.Visible = false;
+
+                //single brick
+                XmlNode brickNode = doc.CreateElement("brick");
+                    //x
+                    XmlAttribute xAttribute = doc.CreateAttribute("x");
+                    xAttribute.Value = $"{label.Location.X}";
+                    brickNode.Attributes.Append(xAttribute);
+                    //y
+                    XmlAttribute yAttribute = doc.CreateAttribute("y");
+                    yAttribute.Value = $"{label.Location.Y}";
+                    brickNode.Attributes.Append(yAttribute);
+                    //width
+                    XmlAttribute widthAttribute = doc.CreateAttribute("width");
+                    widthAttribute.Value = $"{label.Width}";
+                    brickNode.Attributes.Append(widthAttribute);
+                    //height
+                    XmlAttribute heightAttribute = doc.CreateAttribute("height");
+                    heightAttribute.Value = $"{label.Height}";
+                    brickNode.Attributes.Append(heightAttribute);
+                    //value
+                    XmlAttribute valueAttribute = doc.CreateAttribute("value");
+                    valueAttribute.Value = $"{label.Text}";
+                    brickNode.Attributes.Append(valueAttribute);
+                    //colour
+                    XmlAttribute colourAttribute = doc.CreateAttribute("colour");
+                    colourAttribute.Value = $"{label.BackColor.Name}";
+                    brickNode.Attributes.Append(colourAttribute);
+                //close the brick node
+                bricksNode.AppendChild(brickNode);
+            }
+            doc.Save($"lvl{lvlNum}.xml");
+        }
+
+        private void ReadXml()
+        {
+            XmlReader reader = XmlReader.Create($"lvl{lvlNum}.xml");
+            while(reader.Read())
+            {
+                reader.ReadToFollowing("brick");
+                brickX.Add(Convert.ToInt32(reader.GetAttribute("x")));
+                brickY.Add(Convert.ToInt32(reader.GetAttribute("y")));
+                brickWidth.Add(Convert.ToInt32(reader.GetAttribute("width")));
+                brickHeight.Add(Convert.ToInt32(reader.GetAttribute("height")));
+            }
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            for(int i = 0; i<brickX.Count; i++) 
+            { e.Graphics.FillRectangle(red, brickX[i], brickY[i], brickWidth[i], brickHeight[i]);  }
+        }
+    }
+}
